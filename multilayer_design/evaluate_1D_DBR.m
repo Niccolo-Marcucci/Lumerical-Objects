cbb11aed615fe090ed0d4b24db49eb9b03fd2af4
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2020-03-26 01:01:39 +0200

    new design using an invented high refractive index material
     dispersion plot now takes into account of Brewster's angle

--- multilayer_design/evaluate_1D_DBR.m
+++ multilayer_design/evaluate_1D_DBR.m
@@ -1,25 +1,25 @@
 clear
 close all
 addpath functions
-design_file = 'designs/design_TM_guided.mat';
+design_file = 'designs/design_TM7_thickDesign.mat';
 load(design_file);
 
 theta_v=10;
-lambda_v=linspace(400,700,1e5)*1e-9;
-d=zeros(201,1);
-n=zeros(201,1);
+lambda_v=linspace(500,650,1e5)*1e-9;
+d=zeros(121,1);
+n=zeros(121,1);
 
 kB = 2*pi/570e-9*n_eff3;
 kA = 2*pi/570e-9*n_eff1;
 FF = 0.5;
-period = 187e-9;%2* pi/(kB*FF+kA*(1-FF));
+period = 271e-9;%2* pi/(kB*FF+kA*(1-FF));
 da=period*FF;
 db=period*(1-FF);
 
 d(1)=1e-6;
 d(2:2:end)=da;
 d(3:2:end)=db;
-d(end/2+1/2)=175e-9;
+d(end/2+1/2)=280e-9;
 d(end)=1e-6;
 n(1)=n_eff3;
 n(2:2:end)=n_eff1;
@@ -30,22 +30,26 @@ n(3:2:end)=n_eff3;
 [dr,nr,~,~] = prepare_multilayer(d,n);
 
 R=zeros(length(lambda_v),length(theta_v));
-for i=1:length(lambda_v)
+tic
+parfor i=1:length(lambda_v)
     lambda=lambda_v(i);
     R(i,:)=reflectivity(lambda,theta_v,dr,nr,'s');
 end
+toc
 plot(lambda_v*1e9,R)
+title(strcat("Period ",string(period*1e9),"nm, Spacer ",...
+                       string(d(end/2+1/2)*1e9),"nm"))
 nicePlot
 xlabel('wavelength [nm]')
 ylabel('reflectance')
-xlim([400 700])
+xlim([500 650])
 
-[z,nz]=field_distribution(570,0,d,n);
-figure
-plot(z,nz)
-nicePlot
+% [z,nz]=field_distribution(570,0,d,n);
+% figure
+% plot(z,nz)
+% nicePlot
 
 folder="~/OneDrive/BSW/Pictures/";
-name=strcat(folder,"TM_guided_DBR_reflectance");
-% stopBeforeSaving(name)
-% saveas(figure(1),name,'png')
+name=strcat(folder,"TM7_thickDesign_DBR_reflectance");
+stopBeforeSaving(name)
+saveas(figure(1),name,'png')
--- multilayer_design/evaluate_design_dispersion.m
+++ multilayer_design/evaluate_design_dispersion.m
@@ -1,40 +1,70 @@
-clear, close all
-tic
-theta_v = linspace(0,90,5e3);                    % angle vector
+clear, 
+% close all
+
+theta_v = [linspace(0,90,5e3)];% , asin(linspace(1,2,2e3))/pi*180                   
 lambda_v=linspace(400,800,5e3)*1e-9;
 addpath('functions');
 
-load designs/design_TM_guided.mat
+load designs/design_TM7_thickDesign.mat
 pol='p';
 
-[d1,n1,~,~] = prepare_multilayer(d_layers,idx_layers);
+[d,n,~,~] = prepare_multilayer(d_layers,...
+                             idx_layers);%(end:-1:1)
 
 R=zeros(length(lambda_v),length(theta_v));
 parfor i=1:length(lambda_v)
     lambda=lambda_v(i);
-    [R(i,:),~]=reflectivity(lambda,theta_v,d1,n1,pol);
+    [R(i,:),~]=reflectivity(lambda,theta_v,d,n,pol);
 end
-
+R(R>1)=1;
 %%
 [theta,lambda]=meshgrid(theta_v,lambda_v);
 
-beta=sin(theta/180*pi)*n1(1)*2*pi./lambda*(d1(3)-d1(2));
+beta=real(sin(theta/180*pi))*2*pi*n(1)./lambda;
 c=physconst('lightspeed');
-figure('units','normalized','outerposition',[0 0 1 1])
-s=surf(beta,2*pi*c./lambda,1-R);
+f=figure('units','normalized','outerposition',[0 0 1 1]);
+omega=2*pi*c./lambda;
+s=surf(beta,omega,1-R);
 ylabel('angular frequency')
 xlabel('beta')
 s.EdgeColor='none';
 colormap('hot')
-colorbar
+colorbar;
 view(2)
-whitebg(figure(1),'black'); 
+whitebg(f,'black');
 
+
+% the air line is beta*c/(sin(theta_crit)*n_sub), but the denominator
+% is equal to sin(pi/2)*n_air by def.
+
+n_eq = real((n(2)*n(3))/sqrt(n(2)^2 + n(3)^2));
+theta_crit = real(asin(n(end)/n(1)));
+theta_brew =      asin( n_eq /n(1));
+% theta_brew*180/pi 
+% theta_crit*180/pi
+
+beta = linspace(0,beta(1,end),2);
+omega_crit=c*beta/(sin(theta_crit)*n(1));
+% omega_brew=c*beta/(sin(theta_brew)*n(1));
+omega_brew=c*beta/n_eq;
+
+hold on
+plot3(beta,omega_crit,[1 1],'--w','linewidth',3)
+if pol == 'p'
+    plot3(beta,omega_brew,[1 1],'--g','linewidth',3)
+end
+ylim([omega(end,end) omega(1,1)])
+xlim([0 2.5e7])
+%%
 set(gcf, 'InvertHardCopy', 'off');
 folder="~/OneDrive/BSW/Pictures/";
-name=strcat(folder,"TM_guided_dispersion_beta");
+name=strcat(folder,"TM7_thickDesign_dispersion_beta");
 stopBeforeSaving(name)
 saveas(figure(1),name,'png')
+
+theta2=theta_v;
+theta2(imag(theta2)~=0)=90+abs(imag(theta2(imag(theta2)~=0)));
+[theta,lambda]=meshgrid(real(theta2),lambda_v);
 
 figure('units','normalized','outerposition',[0 0 1 1])
 s=surf(theta,lambda,1-R);
@@ -46,9 +76,15 @@ colorbar
 view(2)
 whitebg(figure(2),'black'); 
 
+hold on
+plot3(theta_crit *[1 1]*180/pi,[lambda_v(1),lambda_v(end)],...
+                                        [1 1],'--w','linewidth',3)
+if pol == 'p'
+   plot3(real(theta_brew)*[1 1]*180/pi,[lambda_v(1),lambda_v(end)],...
+                                        [1 1],'--g','linewidth',3)
+end
 set(gcf, 'InvertHardCopy', 'off');
 folder="~/OneDrive/BSW/Pictures/";
-name=strcat(folder,"TM_guided_dispersion_theta");
+name=strcat(folder,"TM7_thickDesign_dispersion_theta");
 saveas(figure(2),name,'png')
 %%
-toc
